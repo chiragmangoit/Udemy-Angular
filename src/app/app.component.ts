a50534eae454c1b74a326c4c79966cdfb6d55b4c
Chirag Bajaj <chirag.mangoit@gmail.com>
2022-06-13 12:10:29 +0530

    jun13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-third-project';
+  loadedFeature = 'recepie'
+
+  onNavigate(feature:string){
+    this.loadedFeature = feature;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShopingComponent } from './shoping/shoping.component';
+import { ShoppingListComponent } from './shoping/shopping-list/shopping-list.component';
+import { RecepieComponent } from './recepie/recepie.component';
+import { RecepieListComponent } from './recepie/recepie-list/recepie-list.component';
+import { RecepieItemComponent } from './recepie/recepie-list/recepie-item/recepie-item.component';
+import { RecepieDetailsComponent } from './recepie/recepie-details/recepie-details.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [			
+    AppComponent,
+    HeaderComponent,
+    ShopingComponent,
+    ShoppingListComponent,
+    RecepieComponent,
+    RecepieListComponent,
+    RecepieItemComponent,
+    RecepieDetailsComponent
+   ],
   imports: [
     BrowserModule
   ],
